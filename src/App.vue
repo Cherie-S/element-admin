13bfbaec88c42655195a573dc15f6be8068c9f97
shendaxue <shendaxue@inspur.com>
2019-11-23 16:44:33 +0800

    fix: add login page
    this login-bg.jpg is from https://github.com/iview/iview-admin.git

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,25 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header height="90px" class="header-top-menu">
-        <e-header/>
-      </el-header>
-      <el-container>
-        <el-aside width="200px">
-          <nav-menu></nav-menu>
-        </el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
+    <login></login>
   </div>
 </template>
 
 <script>
-  import NavMenu from '@/components/NavMenu'
-  import Header from '@/components/Header'
+  import Login from '@/components/login'
   export default {
     name: 'App',
     components: {
-      'nav-menu': NavMenu,
-      'e-header': Header
+      'login': Login
     }
   }
 </script>
 
 <style>
-/*#app {*/
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-/*}*/
-/*.el-container {*/
-  /*height: 600px;*/
-/*}*/
-.header-top-menu{
-  background-color: #4d99e2;
-  color: #fff;
-  line-height: 90px;
-}
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /*color: #2c3e50;*/
+    height: 100%;
+  }
 </style>
---
+++ src/components/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <div class="login-con">
+      <login-form></login-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LoginForm from '@/components/login/login-form'
+  export default {
+    name: 'index',
+    components: {
+      'login-form': LoginForm
+    }
+  }
+</script>
+
+<style scoped>
+  .login {
+    width: 100%;
+    height: 100%;
+    background-image: url('../../assets/images/login-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    position: relative;
+  }
+  .login-con{
+    position: absolute;
+    right: 20%;
+    top: 55%;
+    transform: translateY(-60%);
+    width: 300px;
+  }
+</style>
